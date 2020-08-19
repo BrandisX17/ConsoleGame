a3bd09e934df9628676a339b9f6c0fd86ad0c2c9
BranislavKolar <branokolar@gmail.com>
2020-08-19 17:11:49 +0200

    Testing main

---
+++ src/sk/brano/Main.java
@@ -0,0 +1,11 @@
+package sk.brano;
+
+import sk.brano.gameLogic.Game;
+
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.startGame();
+
+    }
+}
